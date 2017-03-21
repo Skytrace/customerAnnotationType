8f2df0f739e01464ba59152a11d9ca4aff6fcd1b
Andrii_Soboliev <Andrii_Soboliev@epam.com>
2017-03-21 13:25:10 +0200

    add static

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import api.Usage;
 public class Main {
     @Usage(app = SpecificApplication.Facebook, xpath = "//div[@id='facebook-form']")
     @Usage(app = SpecificApplication.Vk, xpath = "//div[@id='vk-form']")
-    private RuntimeXpath xpath;
+    private static RuntimeXpath xpath;
 
     public static void main(String... args) {
         Main main = new Main();
