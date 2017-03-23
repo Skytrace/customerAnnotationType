0e3f5e5a6c6006269d67cf215ef1701d9ff05838
Andrii_Soboliev <Andrii_Soboliev@epam.com>
2017-03-23 18:45:57 +0200

    add test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import run.Main;
+
+public class MainTest {
+
+
+    @Test
+    public void test1() {
+        Main main = new Main();
+        main.testAnnotation();
+    }
+}
